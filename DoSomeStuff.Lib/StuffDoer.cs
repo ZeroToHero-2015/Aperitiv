5b703a6df62f71c330aba165b1d598d3f15d7327
Victor Grigoriu <vgrigoriu@gmail.com>
2015-05-03 20:00:55 +0300

    Lib

---
+++ DoSomeStuff.Lib/StuffDoer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DoSomeStuff.Lib
+{
+    public class StuffDoer
+    {
+        public string GetStuff()
+        {
+            Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+            return "abracadabra";
+        }
+
+        public async Task<string> GetStuffAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            return "abracadabra";
+        }
+    }
+}
